dc764a942b04482ec95d994d2511da7829019f2f
Wesley.Peng <wesley.peng@live.com>
2017-12-21 22:06:00 +0800

    Updated copyright information

--- src/main/python/taf/foundation/plugins/web/selenium/controls/table.py
+++ src/main/python/taf/foundation/plugins/web/selenium/controls/table.py
@@ -1,4 +1,4 @@
-# Copyright 2017 {Flair} WESLEY PENG
+# Copyright (c) 2017-2018 {Flair Inc.} WESLEY PENG
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
